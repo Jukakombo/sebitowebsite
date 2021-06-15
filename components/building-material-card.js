3f6fd898fd26adae00fb5da36edf5def06c2f560
Jukakombo <alisonmagid2016@gmail.com>
2021-06-15 18:36:31 +0100

    just do it

--- components/building-material-card.js
+++ components/building-material-card.js
@@ -6,17 +6,15 @@ const BuildingMaterialCard = ({ data }) => {
   const { image, name, designation } = data;
 
   return (
-    <a href="/bmfcontent">
-      <div className="team-one__single">
-        <div className="team-one__image">
-          <img src={image} alt={name} />
-        </div>
-        <div className="team-one__content">
-          <h3>{name}</h3>
-          <p>{designation}</p>
-        </div>
+    <div className="team-one__single">
+      <div className="team-one__image">
+        <img src={image} alt={name} />
       </div>
-    </a>
+      <div className="team-one__content">
+        <h3>{name}</h3>
+        <p>{designation}</p>
+      </div>
+    </div>
   );
 };
 
