5383fa26f7cdef22b3db77518ac65d3ff8bd8d44
Jukakombo <alisonmagid2016@gmail.com>
2021-06-15 13:46:24 +0100

    still continue

--- utils/dbconnect.js
+++ utils/dbconnect.js
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
-const connection = {};
+// import mongoose from "mongoose";
+// const connection = {};
 
-async function dbConnect() {
-  if (connection.isConnected) {
-    return;
-  }
-  const db = await mongoose.connect(
-    "mongodb+srv://Alison-Web2020:JGIbvkTdxRGjSfAi@cluster0.fgp1w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  );
-  connection.isConnected = db.connections[0].readyState;
-  console.log(connection.isConnected);
-}
-export default dbConnect;
+// async function dbConnect() {
+//   if (connection.isConnected) {
+//     return;
+//   }
+//   const db = await mongoose.connect(
+//     "mongodb+srv://Alison-Web2020:JGIbvkTdxRGjSfAi@cluster0.fgp1w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     }
+//   );
+//   connection.isConnected = db.connections[0].readyState;
+//   console.log(connection.isConnected);
+// }
+// export default dbConnect;
